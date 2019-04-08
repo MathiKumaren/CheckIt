f286beb9aefdadba8b34840a064a11cf76c2305c
MathiKumaren <45756369+MathiKumaren@users.noreply.github.com>
2019-04-08 11:03:38 +0530

    Add files via upload

---
+++ NugetConsole/NugetConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NugetClass.nugetCls cls = new NugetClass.nugetCls();
+            string f = string.Empty;
+            cls.log(f);
+        }
+    }
+}
